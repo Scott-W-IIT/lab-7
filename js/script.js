b3680bc5634db7a6f524b4aabdcefdc724f17d8f
Scott <swilson18@hark.iit.edu>
2019-11-18 21:02:14 -0600

    Update script.js
    added some weird javascript to try and get something to work

--- js/script.js
+++ js/script.js
@@ -1,11 +1,22 @@
 function init(){
-//add your javascrip between these two lines of code
- 
+  
+var fc = document.getElementById('entryinput')
+		//use fc.value in inspector to see the input value
+		alert(fc.value);
+		fc.value = "words";	
+
+  document.getElementById("textoutput").innerHTML = fc.value;
+}
 
 
 
 
-
+var x = document.getElementById('container');
+x.addEventListener('click', init);
+x.addEventListener('click', function(){
+  console.log('I was Clicked!');
+  x.innerHTML = 'entryinput';
+		});
 
 
 window.addEventListener('load', init);
