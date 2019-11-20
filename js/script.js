88ef300b2c06a4a5b95717db857909f14bf158bc
Scott <swilson18@hark.iit.edu>
2019-11-19 20:05:19 -0600

    Update script.js
    formatting

--- js/script.js
+++ js/script.js
@@ -1,12 +1,14 @@
-function init(){
+function init() {
   document.getElementById("entrybutton").addEventListener("click", myalert);
   document.getElementById("entrybutton").addEventListener("click", output);
-} 
-function myalert(){
+}
+function myalert() {
   alert("Scott's Website: " + document.getElementById("entryinput").value);
 }
-function output(){
-  document.getElementById("textoutput").innerHTML = document.getElementById("entryinput").value;
+function output() {
+  document.getElementById("textoutput").innerHTML = document.getElementById(
+    "entryinput"
+  ).value;
 }
 
-window.addEventListener('load', init);
+window.addEventListener("load", init);
