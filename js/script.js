e008fdae096e7360165328d6e6339fcfd8c6e153
Scott <swilson18@hark.iit.edu>
2019-11-20 19:13:37 -0600

    Update script.js
    moved stuff around

--- js/script.js
+++ js/script.js
@@ -1,11 +1,14 @@
 function init() {
-   var button = document.getElementById('entrybutton');
+  var button = document.getElementById('entrybutton');
   var text = document.getElementById('entryinput');
   var showresults = document.getElementById('textoutput');
 
   button.addEventListener('click', function() {
-    window.alert('Scotts website says: ' + text.var);
+    window.alert('Scotts website says: ' + text.value);
   });
 
   button.addEventListener('click', function() {
-    showresults.innerHTML = text.entryinput;
+    showresults.innerHTML = text.value;
+    });
+}
+window.addEventListener('load', init);
